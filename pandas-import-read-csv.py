a75e5ada075b5f6eb7b2a53facba178670af1bdf
CamilaGorelik <kamilalove04@gmail.com>
2026-03-02 13:41:31 +0000

    Fix variable name from df to data-file

--- pandas-import-read-csv.py
+++ pandas-import-read-csv.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("student_productivity_distraction_dataset_20000.csv")
+data-file = pd.read_csv("student_productivity_distraction_dataset_20000.csv")
 
-print(df.head())
-print(df.info())
-print(df.describe())
-print(df.isnull().sum())
+print(data-file.head())
+print(data-file.info())
+print(data-file.describe())
+print(data-file.isnull().sum())

ffeca023c96dd69014f809b1a854c7928baf7cc2
Camila Gorelik <kamilalove04@gmail.com>
2026-03-09 12:58:27 +0000

    Add print statement to display column names in data loading function

--- src/Bea_Brown/data_loader.py
+++ src/Bea_Brown/data_loader.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 def load_data():
     df = pd.read_csv(r"M:\2026-4316COMP-group40\data\student_productivity_distraction_dataset_20000.csv")
+    print(df.columns)
     return df

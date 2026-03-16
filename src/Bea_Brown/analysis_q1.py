e682af01243d84728cc132a70eff9b33038091e9
CamilaGorelik <kamilalove04@gmail.com>
2026-03-16 18:44:13 +0000

    Delete src directory

--- src/Bea_Brown/analysis_q1.py
+++
@@ -1,6 +0,0 @@
-"""
-Are there gender differences in productivity levels among students, and if so, what factors contribute to these differences?
-
-Does attendance affect performance, and if so, to what extent?
-
-"""

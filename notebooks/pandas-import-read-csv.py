c6dd52fd7401df9a43a6ce7392b3168edc972e94
CamilaGorelik <kamilalove04@gmail.com>
2026-03-16 18:43:34 +0000

    Delete notebooks directory

--- notebooks/pandas-import-read-csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-dataFile = pd.read_csv("student_productivity_distraction_dataset_20000.csv")
-
-print(dataFile.head())
-print(dataFile.info())
-print(dataFile.describe())
-print(dataFile.isnull().sum())

f7990edb37e017323171ba311813d140c13f424f
Bea Brown <honeybea29032@gmail.com>
2026-04-12 21:37:47 +0100

    Version one gender analysis full

--- src/Bea_Brown/analysis_q1.py
+++ src/Bea_Brown/analysis_q1.py
@@ -10,4 +10,45 @@ def analyse_gender (data):
     Grouping the data by gender and calculating averages
     """
     return data.groupby("gender").mean(numeric_only=True)
-    
+
+    def create_charts(stats):
+        """
+        Creating a few charts for gender comparison
+        """
+        #The final grade chart
+        chart1 = pygal.Bar()
+        chart1.title = "Average Final Grade by Gender"
+
+        for gender, score in stats["final_grade"].items():
+            chart1.add(gender, hours)
+
+        chart1.render_to_file("gender_final_grade.svg")
+
+        #study hours chart
+        chart2 = pygal.Bar()
+        chart2.title = "Study Hours Per Day by Gender"
+
+        for gender, hours in stats["study_hours_per_day"].items():
+            chart2.add(gender, hours)
+
+        chart2.render_to_file("gender_study_hours.svg")
+
+        #Focus Score chart
+        chart3 = pygal.Bar()
+        chart3.title = "Focus Score by Gender"
+
+        for gender, focus in stats["focus_score"].items():
+            chart3.add(gender, focus)
+
+        chart3.render_to_file("gender_focus.svg")
+
+    def main():
+        data = preprocess_data()
+
+        stats = analyse_gender(data)
+        print(stats)
+
+        create_charts(stats)
+
+    if__name__ == "__main__" :
+    main()
